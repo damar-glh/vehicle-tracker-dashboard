f55230b69a1ad79d32c163b5661c9fa9c0c20c06
Damar Galih <damarmuhamad13@gmail.com>
2025-07-25 04:09:47 +0700

    fix: vercel file for deployment and API

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const WEB_NAME = 'Vetracs.'
-export const API_URL = 'http://128.199.106.149'
+export const API_URL = 'https://vehicle-api.damar-galih.tech'
 export const MOBILE_BREAKPOINT = 768
 export const ITEMS_PER_PAGE = 6
